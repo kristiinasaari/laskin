fda483aeba8888b10a05e24e100ae007ca7e3f81
kristiinasaari <74016307+kristiinasaari@users.noreply.github.com>
2022-01-28 14:29:03 +0200

    korjattu tehtävää

--- App.js
+++ App.js
@@ -1,76 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, Alert, TextInput, Text, text1, text2, resultplus, resultminus } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+ {/*Kommenttikenttä*/}
 
 export default function App() {
-  const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+  const [operand1, setOperand1] = useState('');
+  const [operand2, setOperand2] = useState('');
+
+  const initialFocus = useRef(null);
 
 
-
-  function addTogether() {                              //Laskee yhteen
-    const resultplus = text1 + text2;
-    setTotal(resultplus);
-    Alert.alert('Alert', 'Result: ' + resultplus);
+  const calculate = operator => {                         
+    console.log(operand1, operand2, operator);
+    const [number1, number2] = [Number(operand1), Number(operand2)];
+    switch (operator) {
+      case '+':
+        setResult(number1 + number2);
+        break;
+      case '-':
+        setResult(number1 - number2);
+        break;
+    }
+    setOperand1('');
+    setOperand2('');
+    initialFocus.current.focus();
   }
 
-  function subtract() {                                 //Miinustaa kaksi arvoa
-    const resultminus = text1 + text2;
-    setTotal(resultminus);
-    Alert.alert('Alert', 'Result: ' + resultminus);
-  }
-
-  const buttonPressedplus = () => {                     //Plussaa kun painaa
-    Alert.alert('Result: ' + resultplus); 
-  };
-  const buttonPressedminus = () => {                    //Miinusta kun painaa
-    Alert.alert('Result: ' + resultminus); 
-  };
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container}>                           {/*Ei ole määritelty suuntaa joten kaikki menee pystyyn*/}
+      <Text>Result: {result} </Text>                          {/*Result: normi tekstinä, vastaus objektina*/}
 
-      <View style={styles.container}>
-      <Text>Result:</Text>
-      <TextInput style={styles.input} onChangeText={text => setText(text1)} value={text}/>
-      <TextInput style={styles.input} onChangeText={text => setText(text2)} value={text}/>
+      <TextInput style={styles.input} ref={initialFocus}      
+        keyboardType='number-pad'
+        onChangeText={text => setOperand1(text)}
+        value={operand1}>
+      </TextInput>
+
+      <TextInput style={styles.input}                         
+        keyboardType='number-pad'
+        onChangeText={text => setOperand2(text)}
+        value={operand2}>
+      </TextInput>
+
+      <View style={styles.operators}>                         {/*Button on vähän köyhä komponentti, joten erikseen view komponentti jolle määritellään flexdirection row eli tämän sisällä juoksee vasemmalta oikealle*/}
+
+        <View style={styles.buttons}>                         {/*Ensimmäinen nappi*/}
+          <Button title='+' onPress={() => calculate('+')}> </Button>
+        </View>
+
+        <View style={styles.buttons}>                         {/*Toinen nappi*/}
+          <Button title='-' onPress={() => calculate('-')}> </Button>
+        </View>
+
       </View>
 
-      <View style={styles.buttons}>
-        <Button 
-          flexDirection= 'row'
-          onPress={buttonPressedplus} title="+" />
-        <Button 
-          flexDirection= 'row'
-          onPress={buttonPressedminus} title="-" />
-        <StatusBar style="auto" />
-      </View>
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //flexDirection: 'row',
+  container: {                              //expo init pohjasta joten sen tyylit
+    flex: 1,                                //Tarkoittaa sitä että täyttää kaiken sille käytössä olevan tilan
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center',                   //Kaikki pakataan keskelle sekä pysty että vaakasuunnassa, se on niinku lähtökohtana
     justifyContent: 'center',
   },
   input : {
-    width:200  ,
-    //padding:3, 
     borderColor: 'gray', 
     borderWidth: 1,
-    justifyContent:'space-between',
-    //keyboardType: 'numeric',
+    padding: 5,
+    margin: 5,
+    width: '50%'
+  },
+  operators: {                              //Tällä saadaan napit vaakasuuntaan
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
   },
   buttons: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '20%'
   }
 
 });
